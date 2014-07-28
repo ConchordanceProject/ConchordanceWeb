c5aa371aced57aaaf612dd08f74a11de25d71c08
Alex Solo <alex.n.solo@gmail.com>
2014-07-27 23:17:13 -0500

    fixed silly binding of unnecessary variables to the scope. for shame, solo. for shame

--- WebContent/component/fretboardView.js
+++ WebContent/component/fretboardView.js
@@ -3,14 +3,16 @@ angular.module('conchordance')
     return {
         restrict: 'E',
         link: function(scope, element, attrs) {
-            scope.unscaledFretPositions = [0.0, 0.056, 0.109, 0.159, 0.206, 0.251, 0.293, 0.333, 0.370, 0.405, 0.439, 0.470, 0.5, 0.528, 0.555, 0.580, 0.603, 0.625, 0.646, 0.666, 0.685];
-            scope.scaledFretPositions = new Array(scope.unscaledFretPositions.length);
-            scope.scaledFretPositions[0] = 0;
+            this.unscaledFretPositions = [0.0, 0.056, 0.109, 0.159, 0.206, 0.251, 0.293, 0.333, 0.370, 0.405, 0.439, 0.470, 0.5, 0.528, 0.555, 0.580, 0.603, 0.625, 0.646, 0.666, 0.685];
+            this.scaledFretPositions = new Array(this.unscaledFretPositions.length);
+            this.scaledFretPositions[0] = 0;
 
-            scope.canvas = Raphael(element[0], 0, 0, 800, 120);
+            this.canvas = Raphael(element[0], 0, 0, 800, 120);
+
+            var self = this;
 
             scope.$on('instrument-selected', function(event, instrument) {
-                scope.instrument = instrument;
+                self.instrument = instrument;
                 scope.render();
             });
 
@@ -18,15 +20,15 @@ angular.module('conchordance')
                 var width = 800;
                 var height = 120;
 
-                var strings = scope.instrument.tuning.length;
-                var numFrets = scope.instrument.frets;
+                var strings = self.instrument.tuning.length;
+                var numFrets = self.instrument.frets;
 
                 var fretboardWidth = 700;
                 var fretboardHeight = 100;
 
                 for (var f = 1; f<=numFrets+1; ++f) {
-                    scope.scaledFretPositions[f] = (scope.unscaledFretPositions[f]*fretboardWidth/
-                        scope.unscaledFretPositions[numFrets]);
+                    self.scaledFretPositions[f] = (self.unscaledFretPositions[f]*fretboardWidth/
+                        self.unscaledFretPositions[numFrets]);
                 }
 
                 var stringSpacing = fretboardHeight/(strings-1);
@@ -36,29 +38,29 @@ angular.module('conchordance')
 
                 var fretboardTop = (height-fretboardHeight)/2;
 
-                scope.canvas.clear();
-                scope.canvas.rect(0, 0, width, height, 0).attr({fill: "#fff", stroke: "none"});
-                scope.canvas.rect(fretboardLeft, fretboardTop, fretboardWidth, fretboardHeight, 0).attr({fill: "#fff", stroke: "#000"});
+                self.canvas.clear();
+                self.canvas.rect(0, 0, width, height, 0).attr({fill: "#fff", stroke: "none"});
+                self.canvas.rect(fretboardLeft, fretboardTop, fretboardWidth, fretboardHeight, 0).attr({fill: "#fff", stroke: "#000"});
 
                 // frets
                 var fretWidth = 4;
                 for (var f = 1; f<numFrets; ++f) {
-                    var fx = fretboardLeft + scope.scaledFretPositions[f] - fretWidth;
-                    scope.canvas.rect(fx, fretboardTop, fretWidth, fretboardHeight)
+                    var fx = fretboardLeft + self.scaledFretPositions[f] - fretWidth;
+                    self.canvas.rect(fx, fretboardTop, fretWidth, fretboardHeight)
                         .attr({fill: "#888", stroke: "none"});
                 }
 
                 // strings
                 var stringThickness = 3;
                 for (var i = 0; i<strings; ++i) {
-                    scope.canvas.rect(fretboardLeft, fretboardTop+i*stringSpacing-stringThickness/2,
+                    self.canvas.rect(fretboardLeft, fretboardTop+i*stringSpacing-stringThickness/2,
                         fretboardWidth, stringThickness).attr({fill: "#000", stroke: "none"});
-                    scope.canvas.path("M"+fretboardLeft+","+(fretboardTop+i*stringSpacing-stringThickness/2)+"H"+fretboardRight);
-                    scope.canvas.path("M"+fretboardLeft+","+(fretboardTop+i*stringSpacing+stringThickness/2)+"H"+fretboardRight);
+                    self.canvas.path("M"+fretboardLeft+","+(fretboardTop+i*stringSpacing-stringThickness/2)+"H"+fretboardRight);
+                    self.canvas.path("M"+fretboardLeft+","+(fretboardTop+i*stringSpacing+stringThickness/2)+"H"+fretboardRight);
                 }
 
                 // fret-nut
-                scope.canvas.rect(fretboardLeft-10, fretboardTop-5, 10, fretboardHeight+10).attr({fill: "#888", stroke: "none"});
+                self.canvas.rect(fretboardLeft-10, fretboardTop-5, 10, fretboardHeight+10).attr({fill: "#888", stroke: "none"});
 
                 // TODO highlight the positions included in a selected chord
 
