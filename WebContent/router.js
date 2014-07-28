e66f8b45236dc9dacc498c209af19f1cfe9b82a9
Alex Solo <asolo@revolutionehr.com>
2014-07-27 20:04:15 -0500

    Routing with ui-router

---
+++ WebContent/router.js
@@ -0,0 +1,10 @@
+angular.module('conchordance').config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'views/home.html'
+        });
+});
