7187d6c69dc27c03a5bd35e95feb4a0db914cf58
Steve Johnson <steve@abstractcoder.com>
2014-10-25 15:05:08 -0500

    Added support for MIDI chord preview

--- conchordanceModule.js
+++ conchordanceModule.js
@@ -2,6 +2,26 @@ angular.module('conchordance', ['ui.router', 'ngAnimate'])
 .factory('$music', function() {
 	return Music;
 })
+.factory('$midi', function() {
+	MIDI.loader =  {
+		update: function(a, state, percent) {
+			console.log(state);
+		},
+		message: function(msg) {
+			console.log(msg);
+		}
+	}
+	MIDI.loadPlugin({
+		soundfontUrl: "/soundfont/",
+		instrument: "acoustic_guitar_steel",
+		instruments: ["acoustic_guitar_steel"],
+		callback: function() {
+			console.log("Loaded Midi");
+			MIDI.programChange(0, 25);
+		}
+	});
+	return MIDI;
+})
 .config(function($httpProvider) {
     // Enable CORS
     $httpProvider.defaults.useXDomain = true;
